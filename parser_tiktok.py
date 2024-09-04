d0ceb179820d3c306336c22dfb618099300446a6
kiri4ek <k.kirik@mail.ru>
2024-09-04 22:13:41 +0300

    refactor

--- parser_tiktok.py
+++ parser_tiktok.py
@@ -14,27 +14,6 @@ from pynput import keyboard
 # Constants
 URL_TIKTOK_PREFIX = 'https://www.tiktok.com/@'
 
-# Set recursion limit to a higher value
-# sys.setrecursionlimit(99999)  # Adjust as needed
-
-# Initialize Pygame
-pg.init()
-
-# Configure logging
-logging.basicConfig(filename="Output.log", level=logging.INFO)
-logging.info(str(datetime.datetime.now()) + " | " + 
-            "=========================New START")
-
-# Clear the console
-cls = lambda: os.system('cls' if os.name == 'nt' else 'clear')
-cls()
-
-# Get user input
-nik = input("Введите ник пользователя TikTok без @: ")
-url_tiktok = URL_TIKTOK_PREFIX + nik
-logging.info(str(datetime.datetime.now()) + " | " + 
-            "Введите ник пользователя TikTok без @: " + nik)
-
 # Define headers for requests
 HEADERS = {
     'accept': '*/*',
@@ -44,26 +23,44 @@ HEADERS = {
                   'Chrome/98.0.4758.80 Safari/537.36 Edg/98.0.1108.50'
 }
 
+# Initialize Pygame
+pg.init()
+
+# Configure logging
+logging.basicConfig(filename="Output.log", level=logging.INFO)
+logging.info("%s | =========================New START",
+    str(datetime.datetime.now()))
+
+# Clear the console
+cls = lambda: os.system('cls' if os.name == 'nt' else 'clear')
+cls()
+
+# Get user input
+nik = input("Введите ник пользователя TikTok без @: ")
+url_tiktok = URL_TIKTOK_PREFIX + nik
+logging.info("%s |  Введите ник пользователя TikTok без @: %s",
+    str(datetime.datetime.now()), nik)
+
 # Function to handle connection errors
 def handle_connection_error():
     """Handles server connection errors."""
     print("Connection refused by the server..")
-    logging.info(str(datetime.datetime.now()) + " | " + 
-                "Connection refused by the server..")
+    logging.info("%s | Connection refused by the server..",
+        str(datetime.datetime.now()))
 
     random.seed(str(datetime.datetime.now()))
     rand_time = random.uniform(10, 30)
 
     print(f"Let me sleep for {str(rand_time)} seconds")
-    logging.info(str(datetime.datetime.now()) + " | " + 
-                f"Let me sleep for {str(rand_time)} seconds")
+    logging.info("%s | Let me sleep for %s seconds", 
+        str(datetime.datetime.now()), str(rand_time))
     print("ZZzzzz...")
-    logging.info(str(datetime.datetime.now()) + " | " + "ZZzzzz...")
+    logging.info("%s | ZZzzzz...", str(datetime.datetime.now()))
     time.sleep(rand_time)
 
     print("Was a nice sleep, now let me continue...")
-    logging.info(str(datetime.datetime.now()) + " | " + 
-                "Was a nice sleep, now let me continue...")
+    logging.info("%s Was a nice sleep, now let me continue...",
+        str(datetime.datetime.now()))
 
 # Function to get HTML content with retry mechanism
 def get_html(url, par_headers, params=''):
@@ -75,8 +72,8 @@ def get_html(url, par_headers, params=''):
         except Exception as e:
             handle_connection_error()
             print(f"Error during request: {e}")
-            logging.error(str(datetime.datetime.now()) + " | " + 
-                         f"Error during request: {e}")
+            logging.error("%s | Error during request: %s",
+                str(datetime.datetime.now()), e)
     return req
 
 # Function to get a page with error handling
@@ -87,10 +84,9 @@ def get_page(url):
         print('\nОшибка страница ' + url + ' на данный момент '
               'недоступна! Код ошибки: '+ str(html.status_code) + 
               ' Победа близка, продолжаем дальше!\n')
-        logging.info(str(datetime.datetime.now()) + " | " + 
-                    'Ошибка страница ' + url + ' на данный момент '
-                    'недоступна! Код ошибки: '+ str(html.status_code) + 
-                    ' Победа близка, продолжаем дальше!')
+        logging.info("%s | Ошибка страница %s"
+            "на данный момент недоступна! Код ошибки: %s"
+            "Победа близка, продолжаем дальше!", str(datetime.datetime.now()), url, str(html.status_code))
         time.sleep(10)
         html = get_html(url, HEADERS)
     return html
@@ -119,10 +115,11 @@ def uploaded_new_video(new_video):
     print("Наконец-то! Пользователь " + nik + " опубликовал новое "
           "видео по ссылке: " + new_video['URL_Video_TikTok'] + 
           " \nНажми пробел чтобы остановить музыку!\n")
-    logging.info(str(datetime.datetime.now()) + " | " + 
-                "Наконец-то! Пользователь " + nik + " опубликовал новое "
-                "видео по ссылке: " + new_video['URL_Video_TikTok'] + 
-                " \nНажми пробел чтобы остановить музыку!\n")
+    logging.info("%s | +"
+                "Наконец-то! Пользователь %s опубликовал новое "
+                "видео по ссылке: %s"
+                " \nНажми пробел чтобы остановить музыку!\n",
+                str(datetime.datetime.now()), nik, new_video['URL_Video_TikTok'])
     pg.mixer.music.load('Sound/Sound.mp3')
     pg.mixer.music.play(loops=-1)
     with keyboard.Listener(on_release=on_release) as listener:
@@ -140,27 +137,29 @@ def parser():
     html = get_page(url_tiktok)
     if html.status_code != 200:
         print(f'Ошибка №{html.status_code} при получении {url_tiktok}')
-        logging.info(f"{datetime.datetime.now()} | Ошибка №{html.status_code} "
-                    f"при получении {url_tiktok}")
+        logging.info("%s | Ошибка №%s "
+                    "при получении %s",
+                    datetime.datetime.now(), html.status_code, url_tiktok)
     else:
         videos_first = get_content2(html.text)
 
     while True:
         print(f'\nПарсим страницу: {url_tiktok}\n')
-        logging.info(f"{datetime.datetime.now()} | Парсим страницу: "
-                    f"{url_tiktok}\n")
+        logging.info("%s | Парсим страницу: "
+                    "%s\n", datetime.datetime.now(), url_tiktok)
         random.seed(str(datetime.datetime.now()))
         rand_time = random.uniform(10, 30)
         print(f"ZZzzzz...timeout {rand_time} sec")
-        logging.info(f"{datetime.datetime.now()} | ZZzzzz...timeout "
-                    f"{rand_time} sec")
+        logging.info("%s | ZZzzzz...timeout "
+                    "%s sec", datetime.datetime.now(), rand_time)
         time.sleep(rand_time)
 
         html = get_page(url_tiktok)
         if html.status_code != 200:
             print(f'Ошибка №{html.status_code} при получении {url_tiktok}')
-            logging.info(f"{datetime.datetime.now()} | Ошибка №{html.status_code} "
-                        f"при получении {url_tiktok}")
+            logging.info("%s | Ошибка №%s "
+                        "при получении %s",
+                        datetime.datetime.now(), html.status_code, url_tiktok)
         else:
             videos_second = get_content2(html.text)
 
@@ -174,22 +173,23 @@ def parser():
                     uploaded_new_video(videos_second[0])
                 else:
                     print("Все как обычно ничего нового\n")
-                    logging.info(f"{datetime.datetime.now()} | Все как "
-                                f"обычно ничего нового")
+                    logging.info("%s | Все как "
+                                "обычно ничего нового",
+                                datetime.datetime.now())
             else:
                 print(f"У пользователя {nik} нету видео или вернулась "
                       "пустая строка попробуем еще...\n")
-                logging.info(f"{datetime.datetime.now()} | У пользователя {nik} "
+                logging.info("%s | У пользователя %s "
                             "нету видео или вернулась пустая строка "
-                            "попробуем еще...")
+                            "попробуем еще...", datetime.datetime.now(), nik)
 
 # Function for keyboard listener 
 def on_release(key):
     """Stops playing music by pressing the space bar."""
     if key == keyboard.Key.space:
         print("Слежка за " + nik + " продолжилась...\n")
-        logging.info(str(datetime.datetime.now()) + " | " + 
-                    "Слежка за " + nik + " продолжилась...")
+        logging.info("%s | Слежка за %s продолжилась...",
+                    str(datetime.datetime.now()), nik)
         return False
 
 # Main execution block
@@ -198,9 +198,9 @@ if __name__ == "__main__":
         parser()
     except KeyboardInterrupt:
         print('Прасинг прерван пользователем!')
-        logging.warning(f"{datetime.datetime.now()} | Прасинг прерван "
-                        "пользователем!")
+        logging.warning("%s | Прасинг прерван "
+                        "пользователем!", datetime.datetime.now())
     except Exception as e:
         print(e)
-        logging.error(f"{datetime.datetime.now()} | {e}")
+        logging.error("%s | %s", datetime.datetime.now(), e)
         traceback.print_tb(e.__traceback__)
